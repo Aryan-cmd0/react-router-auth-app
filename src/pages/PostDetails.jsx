5ee0ea6693d3593511e04e278d05cb9925d38ef3
Aryan-cmd0 <215001262+Aryan-cmd0@users.noreply.github.com>
2026-04-03 23:41:00 +0530

    Added comments section to post details using API

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
+import { useParams, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
-    const navigate = useNavigate()
-    const { id } = useParams();
-    const [post, setPost] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then((res) => res.json())
-            .then((data) => setPost(data));
-    }, [id]);
+  const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
 
-    if (!post) return <h2>Loading...</h2>;
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
 
-    return (
-        <div>
-            <button onClick={() => navigate(-1)}> ⬅ Back</button>
-            <h1>Post Details</h1>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, [id]);
+
+  if (!post) return <h2>Loading...</h2>;
+
+  return (
+    <div>
+      <button onClick={() => navigate(-1)}>⬅ Back</button>
+
+      <h1>Post Details</h1>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+
+      <h2>Comments</h2>
+
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          <h4>{comment.name}</h4>
+          <p>{comment.body}</p>
+          <hr />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default PostDetails;
