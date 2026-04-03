64063b85535549bea82fd4a539f9d44cb4ac08e7
Aryan-cmd0 <215001262+Aryan-cmd0@users.noreply.github.com>
2026-04-03 19:53:28 +0530

    Added back navigate button to post details page

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 const PostDetails = () => {
-  const { id } = useParams();
-  const [post, setPost] = useState(null);
+    const navigate = useNavigate()
+    const { id } = useParams();
+    const [post, setPost] = useState(null);
 
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((res) => res.json())
-      .then((data) => setPost(data));
-  }, [id]);
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then((res) => res.json())
+            .then((data) => setPost(data));
+    }, [id]);
 
-  if (!post) return <h2>Loading...</h2>;
+    if (!post) return <h2>Loading...</h2>;
 
-  return (
-    <div>
-      <h1>Post Details</h1>
-      <h2>{post.title}</h2>
-      <p>{post.body}</p>
-    </div>
-  );
+    return (
+        <div>
+            <button onClick={() => navigate(-1)}> ⬅ Back</button>
+            <h1>Post Details</h1>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        </div>
+    );
 };
 
 export default PostDetails;
